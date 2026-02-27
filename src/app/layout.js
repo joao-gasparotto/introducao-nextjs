2e43783e15b61d6f2a955a3ff79cef99b8404f5a
JOAO VITOR BRAZ GASPAROTTO <joao.gasparotto>
2026-02-27 13:49:11 -0300

    aula css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./estilizacao/globals.css";
+import"./estilizacao/reset.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,9 @@ function Home() {
   return (
     <div>
       <h1>React com Next JS</h1>
+      <h2>React com oq sobra pro beta</h2>
+      <h3>Não sei oque falar</h3>
+      <p>jajajaja classe sd</p>
     </div>
   );
 }
