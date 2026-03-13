74b4f2bfeda9cfef392b303112de8fde0fffc6f0
joao-gasparotto <willfsevermine@gmail.com>
2026-03-13 11:05:47 -0300

    Aula-06-03
    Aula-06-03

---
+++ src/app/Ex-01/pagemodule.jsx
@@ -0,0 +1,6 @@
+function Exemplo 1 (){
+     return
+     <div>
+        <h1>Exemplo 1</h1>
+     </div>
+}
--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,17 @@ import styles from "./page.module.css";
 
 function Home() {
   return (
-    <div>
+    <div className={styles.containerExemplo}>
       <h1>React com Next JS</h1>
-      <h2>React com oq sobra pro beta</h2>
-      <h3>Não sei oque falar</h3>
-      <p>jajajaja classe sd</p>
+      <h2>Aula de introdução</h2>
+      <h3>Leiaute e Estilização</h3>
+      <p>Nesta aula iremos entender o uso de CSS global e local.</p>
+      <label>Exemplo Label</label>
+      <small>Exemplo Small</small>
+      <div className={styles.conteinerExPosicao}>
+        <div className={styles.divVermelha}></div>
+        <div className={styles.divVerde}></div>
+      </div>
     </div>
   );
 }
