892caf0a6f0227197d0ddf4963adbd67b0c089dc
joao-gasparotto <willfsevermine@gmail.com>
2026-03-16 14:48:49 -0300

    aula do aura

---
+++ src/app/Exemplos/Ex01/page.jsx
@@ -0,0 +1,10 @@
+function exemplo01 () {
+    return (
+        <div>
+            <h1>Exemplo 01</h1>
+            <p>cComponente básico para renderizar tela com React</p>
+        </div>
+    )
+}
+
+export default exemplo01;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import styles from "./page.module.css"
 
 function Home() {
     return(
         <div className={styles.containerHome}>
             <div className={styles.containerListas}>
                 <h1>Exemplos</h1>
-            </div>
+                <Link href="./Exemplos/Ex01">Exemplo 1</Link>
+                <Link href="./Exemplos/Ex02">Exemplo 2</Link>
+            </div>  
             <div className={styles.containerListas}>
                 <h1>Atividades</h1>
+                
             </div>
         </div>
     )
